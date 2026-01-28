9436e0c9a8d180119ea1301f5bfc2d22660b8606
kamalcodezen <kamaluddin7908@gmail.com>
2026-01-29 01:10:03 +0530

    while loop

---
+++ javaScript/2. while-loop.js
@@ -0,0 +1,15 @@
+// ===== while Loop 
+
+let i = 1;
+while (i <= 5) {
+    console.log(i, "kamaluddin");
+    i++;
+
+}
+console.log("i =", i);
+
+let j = 1;
+while (j <= 10) {
+    console.log(j, "kamal");
+    j++;
+}

6909f8021aa181f05d19905bdea8d6301491c4c5
kamalcodezen <kamaluddin7908@gmail.com>
2026-01-29 02:50:48 +0530

    task 1 complete

---
+++ javaScript/6. practice-task.js
@@ -0,0 +1,20 @@
+/************************************
+ * Qs1. print all even number from 0 to 100. *************************************/
+
+// print even number
+for (let num = 0; num <= 100; num++) {
+    if (num % 2 === 0) {
+        console.log("even num =", num);
+    }
+}
+
+
+// print ODD number
+for (let num = 0; num <= 100; num++) {
+    if (num % 2 !== 0) {
+        console.log("odd num =", num);
+    }
+}
+
+// ===================================
+

1e0a2436f3e81d0bd774b2db5f0db093f35f03d5
kamalcodezen <kamaluddin7908@gmail.com>
2026-01-29 15:51:13 +0530

    uppercase and lowercase practic emethod in string

---
+++ javaScript/8. string-method.js
@@ -0,0 +1,32 @@
+/**********************************
+ * string method | these are built in function to manipulate a string
+ ***********************************/
+
+
+// str.toUpperCase()
+
+let fullName = "kamaluddin";
+
+console.log(fullName.toUpperCase());
+// fullName.toUpperCase();
+
+let newName = fullName.toUpperCase();
+console.log(newName);
+
+fullName = fullName.toUpperCase();
+console.log(fullName);
+
+
+
+
+// str.tolowerCase
+
+let name = "kamal";
+
+console.log(name.toLowerCase());
+
+name = name.toLowerCase();
+console.log(name);
+
+let nameNew = name.toLowerCase();
+console.log(nameNew);
